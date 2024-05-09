5d9b36a70d7c4a486f500787129cc856a5ab7055
jeongY <zoe1126@naver.com>
2024-05-09 15:31:32 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,6 @@
 import React from "react";
 
 function App(props) {
-  // object
-  const a = {
-    name: "john",
-  };
-
-  const b = a;
-  console.log(a === b);
-
-  console.log(a.name); // john
-  console.log(b.name); // john
-
-  b.name = "jane";
-  console.log(a === b);
-
-  console.log(a.name); // Jane
-  console.log(b.name); // Jane
-
   return <div></div>;
 }
 
