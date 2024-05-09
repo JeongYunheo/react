7425aaef1c5af9a815e69b5334d8f6d7e835eb5a
jeongY <zoe1126@naver.com>
2024-05-09 15:28:54 +0900

    javascript object도 참조값을 저장한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 
 function App(props) {
+  // object
+  const a = {
+    name: "john",
+  };
+
+  const b = a;
+  console.log(a === b);
+
+  console.log(a.name); // john
+  console.log(b.name); // john
+
+  b.name = "jane";
+  console.log(a === b);
+
+  console.log(a.name); // Jane
+  console.log(b.name); // Jane
+
   return <div></div>;
 }
 
