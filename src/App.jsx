19d9bcfd8b0b5da74392c47ef8d82862876088ce
jeongY <zoe1126@naver.com>
2024-05-09 11:10:13 +0900

    component 사용 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+function MyTag() {
+  return <h3>hello component</h3>;
+}
+
+function MyContent() {
+  return <p> my content </p>;
+}
+
 function App() {
   return (
     <>
       <div>
         <h1>hello react</h1>
+        <MyTag />
+        <MyTag />
+        {/* <h3>hello component</h3> */}
+        <MyContent />
+        <MyContent />
+        {/* <p> my content </p> */}
       </div>
     </>
   );
