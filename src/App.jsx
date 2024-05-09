7cc66a3746a2e8125dc28aa426645eb675ad08a1
jeongY <zoe1126@naver.com>
2024-05-09 16:34:07 +0900

    javascript String "", '', ``

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // String
+  const s1 = "some string";
+  const s2 = "some string";
+  const s3 = "I'm 우석이에요 ";
+  const s4 = '제 이름은 "흥민"입니다';
+
+  // backtick
+  // template literal
+  const s5 = `another string`;
+
+  const name = "son";
+  const age = 30;
+
+  const s6 = name + " is " + (age + 2) + " years old";
+  const s7 = `${name} is ${age * 2} years old`;
+
+  return (
+    <div>
+      <p>{s6}</p>
+      <p>{s7}</p>
+    </div>
+  );
 }
 
 export default App;
