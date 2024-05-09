87a01be2dd249223f5a303a03ebad789a6d36b5e
jeongY <zoe1126@naver.com>
2024-05-09 12:25:53 +0900

    jsx 내에서 {} 활용 코드 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 function App() {
-  return <></>;
+  const a = 5;
+  const b = 9;
+
+  function someMethod() {
+    return "hello js";
+  }
+
+  function someMethod2(p) {
+    return p + " " + p;
+  }
+
+  return (
+    <>
+      <div>
+        {/* <p> 5 x 9 = 45 </p> */}
+        <p>
+          {a} x {b} = {a * b}
+        </p>
+        <p>{someMethod()}</p> {/* hello js */}
+        <p>{someMethod2("react")}</p> {/* react react*/}
+      </div>
+    </>
+  );
 }
 
 export default App;
