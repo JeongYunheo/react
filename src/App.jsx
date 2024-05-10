63af2b46b1ab3cdaef565cff1bb960cb65222492
jeongY <zoe1126@naver.com>
2024-05-10 15:11:51 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  return <div></div>;
-}
-
 function App(props) {
-  return (
-    <div>
-      {/* props의 값 타입은 js 값 타입*/}
-      {/* {}에 값 입력, (string 만 생략 가능)*/}
-      <MyComp
-        name={"son"}
-        string="gangnam"
-        age={3.14}
-        address={{ city: "seoul", country: "USA" }}
-        foods={["pizza", "coffee"]}
-        married={true}
-        action={function () {
-          console.log("action");
-        }}
-        someAction={() => console.log("some action")}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
