a5b74e69376264ca54f7783951a78109ac0405bc
jeongY <zoe1126@naver.com>
2024-05-10 11:28:11 +0900

    분해할당의 나머지 모두 문법

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
 import React from "react";
 
 function App(props) {
+  // object ({},[])
+  // 나머지 모두(rest property)
+  // ...변수명
+
+  const a = {
+    name: "john",
+    email: "john@email.com",
+    password: "password@email.com",
+    address: "gangnam",
+  };
+
+  const { name, email, ...b } = a;
+  console.log("name", name);
+  console.log("email", email);
+  console.log("b.password", b.password); // "password"
+  console.log("b.address", b.address); //"gangnam"
+
+  // 연습:
+  const c = {
+    city: "seoul",
+    country: "USA",
+    price: 500,
+    category: "food",
+  };
+
+  const { price, ...d } = c;
+  console.log("price", price); // 500
+  console.log("d.city", d.city); // seoul
+  console.log("d.country", d.country); // usa
+  console.log("d.category", d.category); // food
+
   return <div></div>;
 }
 
