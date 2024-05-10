29173023cb65faab6a8a0a2f655cbdbe25c3be9a
jeongY <zoe1126@naver.com>
2024-05-10 14:30:36 +0900

    props를 분해할당으로 받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-function App(props) {
+function MyElem(props) {
+  console.log(props);
+
+  const { name, age } = props;
+  console.log("age", age);
+  console.log("name", name);
+
   return <div></div>;
+}
+
+function MyComp({ city, country, price }) {
+  // props : {city:"seoul", country:"korea", price:500}
+  console.log("city", city);
+  console.log("country", country);
+  console.log("price", price);
+  return <div></div>;
+}
+function App(props) {
+  return (
+    <div>
+      <MyElem name="son" age={33} />
+      <MyComp city="seoul" country="USA" price={500} />
+    </div>
+  );
 }
 
 export default App;
