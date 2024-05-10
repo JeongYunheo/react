423e5d378c705c779d3eadf067d1f19182425f9d
jeongY <zoe1126@naver.com>
2024-05-10 14:38:56 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import React from "react";
 
-function MyElem(props) {
-  console.log(props);
-
-  const { name, age } = props;
-  console.log("age", age);
-  console.log("name", name);
-
-  return <div></div>;
-}
-
-function MyComp({ city, country, price }) {
-  // props : {city:"seoul", country:"korea", price:500}
-  console.log("city", city);
-  console.log("country", country);
-  console.log("price", price);
-  return <div></div>;
-}
 function App(props) {
-  return (
-    <div>
-      <MyElem name="son" age={33} />
-      <MyComp city="seoul" country="USA" price={500} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
