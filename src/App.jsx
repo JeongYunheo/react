612f39055d04010b069e346a2f9a5bb93a942c2d
jeongY <zoe1126@naver.com>
2024-05-13 11:03:46 +0900

    코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
-import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <Button colorScheme={"blue"}>클릭</Button>
-        <Button colorScheme={"green"} variant="outline">
-          알림
-          <Badge variant="solid" colorScheme={"green"}>
-            new
-          </Badge>
-        </Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
