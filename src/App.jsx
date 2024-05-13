a49c5e6b4fe7e3f1a49496a800acc08715bee7f4
jeongY <zoe1126@naver.com>
2024-05-13 11:02:52 +0900

    chakra ui component 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <Button colorScheme={"blue"}>클릭</Button>
+        <Button colorScheme={"green"} variant="outline">
+          알림
+          <Badge variant="solid" colorScheme={"green"}>
+            new
+          </Badge>
+        </Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
