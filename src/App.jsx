c40c22d18be557dfdb3c382fdb8a7320c516c1b0
jeongY <zoe1126@naver.com>
2024-05-14 09:29:55 +0900

    component는 각자 자기의 상태만 갖는다 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox() {
+  const [content, setContent] = useState("");
+
+  function handelChange(e) {
+    setContent(e.target.value);
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handelChange} />
+      <p>{content}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyBox />
+      <hr />
+      <MyBox />
+      {/*
+    <div>
+    <input />
+    <p></p>
+    </div>
+    두번 나오게
+    */}
+    </div>
+  );
 }
 
 export default App;
