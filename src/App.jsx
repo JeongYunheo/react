3c430e7cdd6e28113f6d36ca8f0af34eb09ce9b5
jeongY <zoe1126@naver.com>
2024-05-14 11:32:47 +0900

    prop drilling > context 사용해서 데이터 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
 
+// step 1: create the context
+const MessageContext = createContext("");
+
+function MyBox({ message }) {
+  return <div>{message}</div>;
+}
+
+function MySection() {
+  // step 2: use the context
+  const message = useContext(MessageContext);
+  return <MyBox message={message} />;
+}
+
+function MyContainer() {
+  return <MySection />;
+}
+
+// context
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState("");
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setMessage(e.target.value)} />
+      <p>{message}</p>
+      <hr />
+      {/* step 3: provide the content */}
+      <MessageContext.Provider value={message}>
+        <MyContainer />
+      </MessageContext.Provider>
+    </div>
+  );
 }
 
 export default App;
