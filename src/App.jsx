d70fceda21574708a09e628f489f7995b9e98350
jeongY <zoe1126@naver.com>
2024-05-16 10:14:59 +0900

    /api/some3의 get ajax 작성

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,22 @@ function App(props) {
     axios.post("/api/someurl");
   }
 
-  function handleClickButton12() {
+  function handleClickButton3() {
     axios.get("/api/someurl2");
   }
 
-  function handleClickButton22() {
+  function handleClickButton4() {
     axios.post("/api/someurl2");
+  }
+
+  function handleClickButton5() {
+    const params = new URLSearchParams();
+    params.append("id", 3);
+    params.append("name", "son");
+    params.append("email", "son@son.com");
+
+    // axios.get("/api/someurl3?" + params);
+    axios.get(`/api/someurl3?${params}`);
   }
 
   return (
@@ -25,8 +35,9 @@ function App(props) {
       <button onClick={handleClickButton1}>get 요청</button>
       <button onClick={handleClickButton2}>post 요청</button>
       {/* /api/someurl2 get, post */}
-      <button onClick={handleClickButton12}>get 요청2</button>
-      <button onClick={handleClickButton22}>post 요청2</button>
+      <button onClick={handleClickButton3}>get 요청2</button>
+      <button onClick={handleClickButton4}>post 요청2</button>
+      <button onClick={handleClickButton5}>get 요청 with query string</button>
     </div>
   );
 }
