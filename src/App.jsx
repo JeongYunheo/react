b88bd81cf120581361196e17e2719e8c24e9e8f4
jeongY <zoe1126@naver.com>
2024-05-16 14:20:09 +0900

    응답받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    axios.get("/api/main42/sub1").then((param) => console.log(param.data));
+  }
+  // then 에는 함수가 들어감
+
+  function handleClick2() {
+    axios.get("/api/main42/sub2").then((param) => console.log(param.data));
+  }
+  return (
+    <div>
+      <button onClick={handleClick1}>응답 받기</button>
+      <br />
+      {/* 버튼이 클릭되면 /api/main42/sub2 get 요청 후
+          응답본문을 콘솔로 출력 */}
+      <button onClick={handleClick2}>응답 받기</button>
+    </div>
+  );
 }
 
 export default App;
