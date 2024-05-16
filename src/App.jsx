81ef76eee94a31da0d08f6948e37a1eafd0067ff
jeongY <zoe1126@naver.com>
2024-05-16 14:29:19 +0900

    JSON 응답 파싱하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [result, setResult] = useState("");
+
   function handleClick1() {
     axios.get("/api/main42/sub1").then((param) => console.log(param.data));
   }
   // then 에는 함수가 들어감
 
   function handleClick2() {
-    axios.get("/api/main42/sub2").then((param) => console.log(param.data));
+    axios.get("/api/main42/sub2").then((param) => setResult(param.data));
   }
+
+  function handleClick3() {
+    axios.get("/api/main42/sub3").then((res) => console.log(res));
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>응답 받기</button>
@@ -17,6 +24,9 @@ function App(props) {
       {/* 버튼이 클릭되면 /api/main42/sub2 get 요청 후
           응답본문을 콘솔로 출력 */}
       <button onClick={handleClick2}>응답 받기</button>
+      <br />
+      <p>{result}</p>
+      <button onClick={handleClick3}>JSON 응답 받기</button>
     </div>
   );
 }
