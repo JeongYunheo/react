2593a65ee355338da2bdb66f911b13d882201549
jeongY <zoe1126@naver.com>
2024-05-16 10:40:40 +0900

    /api/some4의 get, post ajax 작성

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,34 @@ function App(props) {
     axios.get(`/api/someurl3?${params}`);
   }
 
+  function handleClickButton6() {
+    const params = new URLSearchParams();
+    params.append("id", 5);
+    params.append("city", "서울");
+    params.append("country", "한국");
+
+    axios.post("/api/someurl3", params);
+  }
+
+  function handleClickButton7() {
+    const params = new URLSearchParams();
+    params.append("id", 3);
+    params.append("team", "spurs");
+    params.append("position", "mid");
+
+    axios.get(`/api/someurl4?${params}`);
+  }
+
+  function handleClickButton8() {
+    const params = new URLSearchParams();
+    params.append("id", 6);
+    params.append("name", "son");
+    params.append("email", "son@son.com");
+    params.append("email", "son@pon.com");
+
+    axios.post("/api/someurl4", params);
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
@@ -37,7 +65,12 @@ function App(props) {
       {/* /api/someurl2 get, post */}
       <button onClick={handleClickButton3}>get 요청2</button>
       <button onClick={handleClickButton4}>post 요청2</button>
+      {/* data도 함께 보내기*/}
       <button onClick={handleClickButton5}>get 요청 with query string</button>
+      <button onClick={handleClickButton6}>post 요청 with data</button>
+      {/* /api/someurl4 get, post with data */}
+      <button onClick={handleClickButton7}>get request with data</button>
+      <button onClick={handleClickButton8}>post request with data</button>
     </div>
   );
 }
