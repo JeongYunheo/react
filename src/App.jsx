b155ad974e6adf00586817b4ee3d938ff52475d9
jeongY <zoe1126@naver.com>
2024-05-16 12:06:24 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  const [name, setName] = useState("");
-  const [city, setCity] = useState("");
-
-  const [id, setId] = useState("");
-  const [email, setEmail] = useState("");
-
-  function handleClick1() {
-    const obj1 = { name: "son", age: 20 };
-    // const json1 = JSON.stringify(obj1);
-
-    // axios 가 두번째 파라미터 객체면 직렬화 해줌
-    axios.post("/api/main41/sub1", obj1);
-  }
-
-  function handleClick2() {
-    const data = {
-      name: "lee",
-      age: 23,
-    };
-
-    axios.post("/api/main41/sub2", data);
-  }
-
-  function handleClick3() {
-    const data = {
-      //  name: name,
-      //  city: city,
-      name,
-      city,
-    };
-    axios.post("/api/main41/sub3", { name, city });
-  }
-
-  function handleClick4() {
-    const data = {
-      id,
-      email,
-    };
-    axios.post("/api/main41/sub4", data);
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick1}>request (json 데이터 포함)</button>
-      {/* /api/main41/sub2 post */}
-      <button onClick={handleClick2}>요청 (json 데이터 포함)</button>
-      <div>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
-        <br />
-        <input type="text" onChange={(e) => setCity(e.target.value)} />
-        <br />
-        <button onClick={handleClick3}>요청(input에 입력한 값)</button>
-      </div>
-      <div>
-        <input type="text" onChange={(e) => setId(e.target.value)} />
-        <br />
-        <input type="text" onChange={(e) => setEmail(e.target.value)} />
-        <br />
-        <button onClick={handleClick4}>요청(input에 입력한 값)</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
