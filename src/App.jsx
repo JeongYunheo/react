e58164f3444673f2afb492ed7403805c8530e658
jeongY <zoe1126@naver.com>
2024-05-17 16:08:15 +0900

    게시물 홈, 게시물 목록

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+function Root() {
+  return (
+    <div>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to={"/list"}>List</Link>
+      </div>
+      <hr />
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+function BoardList() {
+  return (
+    <div>
+      <div>no.1 보기</div>
+      <div>no.2 보기</div>
+      <div>no.3 보기</div>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <div>MAIN PAGE</div>,
+      },
+      { path: "board", element: <div>BOARD PAGE</div> },
+      { path: "list", element: <BoardList /> },
+    ],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
