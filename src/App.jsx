91a1e5949f90b4a0de6211dd01f046efe6813759
jeongY <zoe1126@naver.com>
2024-05-17 14:08:49 +0900

    react-router 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,99 @@
-function App() {
+import React, { useState } from "react";
+import axios from "axios";
+
+function App(props) {
+  const [username, setUsername] = useState("son");
+  const [scope, setScope] = useState("admin manager");
+
+  function handleLogin() {
+    axios.post("/api/main44/login", { username, scope }).then((res) => {
+      localStorage.setItem("token", res.data);
+    });
+  }
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+  }
+
+  function handleAccessAll() {
+    axios.get("/api/main44/all").then((res) => alert(res.data));
+  }
+
+  function handleAccessUser() {
+    axios
+      .get("/api/main44/user", {
+        // 토큰을 들고 가기 위해서
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => alert(res.data));
+  }
+
+  function handleAccessAdmin() {
+    axios
+      .get("/api/main44/admin", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => alert(res.data));
+  }
+
+  function handleAccessManager() {
+    axios
+      .get("/api/main44/manager", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => alert(res.data));
+  }
+
+  function handleAccessManagerOrAdmin() {
+    axios
+      .get("/api/main44/ma", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => alert(res.data));
+  }
+
   return (
-    <>
+    <div>
       <div>
-        <h1>hello react</h1>
+        <input
+          type="text"
+          defaultValue={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
-    </>
+      <div>
+        <input
+          type="text"
+          defaultValue={scope}
+          onChange={(e) => setScope(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={handleLogin}>로그인</button>
+      </div>
+      <hr />
+      <button onClick={handleLogout}>로그아웃</button>
+      <hr />
+      <button onClick={handleAccessAll}>누구나</button>
+      <hr />
+      <button onClick={handleAccessUser}>로그인한 유저만</button>
+      <hr />
+      <button onClick={handleAccessAdmin}>어드민 경로</button>
+      <hr />
+      <button onClick={handleAccessManager}>매니저 경로</button>
+      <hr />
+      <button onClick={handleAccessManagerOrAdmin}>
+        매니저 또는 어드민 경로
+      </button>
+    </div>
   );
 }
 
