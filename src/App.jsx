ce86ee104ee52faa0a669622b9731ba040590af0
jeongY <zoe1126@naver.com>
2024-05-20 12:25:57 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
-import { DeleteIcon, MoonIcon } from "@chakra-ui/icons";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <DeleteIcon />
-      </div>
-      <div>
-        <Button leftIcon={<MoonIcon />}> moon </Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
