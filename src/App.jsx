b71e2fdff12325f8fd5ea14f66f69a2b9bbeac59
jeongY <zoe1126@naver.com>
2024-05-20 10:21:57 +0900

    hover pseudo class

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box
+        bgColor={"yellow"}
+        _hover={{
+          bgColor: "orange",
+        }}
+      >
+        Lorem ipsum dolor.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
