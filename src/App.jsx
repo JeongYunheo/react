d45272d7f368a2e0db66f153402105740fe88f49
jeongY <zoe1126@naver.com>
2024-05-20 14:33:59 +0900

    기본 동작 막기(preventDefault)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function handleLinkClick(e) {
+    // 기본 이벤트 동작을 막기
+    e.preventDefault();
+
+    // 개발자가 원하는 동작 ...
+  }
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    console.log("prevent submit form");
+  }
+
+  return (
+    <div>
+      <a href="https://www.naver.com" onClick={handleLinkClick}>
+        naver
+      </a>
+      <form action="https://www.naver.com" onSubmit={handleSubmitForm}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
