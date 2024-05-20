8dfb3aa2b20b2ecdb683654db8231d8c3c45a55a
jeongY <zoe1126@naver.com>
2024-05-20 10:16:59 +0900

    반응형 스타일

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box bgColor={"orange"} w={"500px"}>
+        Lorem3
+      </Box>
+      <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
+        Lorem ipsum dolor.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
