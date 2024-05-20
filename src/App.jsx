cd518042dcb4c4bc03de552a7484a1a8c2aa2e7d
jeongY <zoe1126@naver.com>
2024-05-20 14:10:16 +0900

    input 요소 value prop에 값을 주고 변경하는 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { ChakraProvider, useDisclosure, useToast } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 function App(props) {
-  // use... : hook
-  // if / for 내에서 절대 사용하지 말 것
-
-  // hook 은 안전하게 컴포넌트 가장 산단에 순서대로 작성할 것
-
-  const [count, setCount] = useState(0);
-  useEffect(() => {}, []);
-  const toast = useToast();
-  const [isOpen, onClose, onOpen] = useDisclosure();
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("hello");
+  const [text3, setText3] = useState("hi");
+  const [text4, setText4] = useState("안녕");
+  const [text5, setText5] = useState("hola");
 
   return (
-    <ChakraProvider>
-      <div></div>
-    </ChakraProvider>
+    <div>
+      <div>
+        <input type="text" onChange={(e) => setText1(e.target.value)} />
+      </div>
+      <div>
+        <input type="text" value={text2} readOnly={true} />
+      </div>
+      <div>
+        <input type="text" defaultValue={text3} />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={text4}
+          onChange={(e) => setText4(e.target.value)}
+        />
+      </div>
+      <div>
+        <input type="text" value={text5} />
+      </div>
+    </div>
   );
 }
 
