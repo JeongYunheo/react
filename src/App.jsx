517ca134c56b829dda9a5b200d11165ea0ca0110
jeongY <zoe1126@naver.com>
2024-05-20 11:02:57 +0900

    FormControl

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App(props) {
       <label htmlFor="input1">이름</label>
       <input id={"input1"} />
       <hr />
-      <Center w={{ base: "100%", md: "50%" }}>
-        <Box>
+      <Center>
+        <Box w={{ base: "100%", md: "50%" }}>
           <FormControl>
             <FormLabel>이름</FormLabel>
             <Input />
