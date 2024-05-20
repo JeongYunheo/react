2b17cb85406bfc290a643b13a6ccb55b6d856876
jeongY <zoe1126@naver.com>
2024-05-20 10:07:00 +0900

    스페이스

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box m={1} bgColor={"orange"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box m={2} bgColor={"orange"}>
+        Facilis mollitia, tempora?
+      </Box>
+      <Box m={10} bgColor={"orange"}>
+        Incidunt, omnis, unde!
+      </Box>
+      <Box m={20} bgColor={"orange"}>
+        Dolor fuga, maxime.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
