e37e73a42d68203dcde3379923906d0bd32188a1
jeongY <zoe1126@naver.com>
2024-05-20 10:08:04 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Box m={1} bgColor={"orange"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box m={2} bgColor={"orange"}>
-        Facilis mollitia, tempora?
-      </Box>
-      <Box m={10} bgColor={"orange"}>
-        Incidunt, omnis, unde!
-      </Box>
-      <Box m={20} bgColor={"orange"}>
-        Dolor fuga, maxime.
-      </Box>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
